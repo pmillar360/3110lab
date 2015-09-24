7d0d55411f878376821e269670aaa391dfa3b851
patrickmillar <patrickmillar@5010-036.ODENG2.CARLETON.CA>
2015-09-24 12:08:33 -0400

    push

---
+++ src/AddressBook.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+	
+	private ArrayList<BuddyInfo> abList;
+	
+	public AddressBook()
+	{
+		this.abList = new ArrayList<BuddyInfo>();
+
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("Address Book");
+		AddressBook ab = new AddressBook();
+		BuddyInfo bud = new BuddyInfo("Nome", "123 Address", "967-1111");
+		ab.addBuddy(bud);
+		ab.removeBuddy(0);
+	}
+	
+	public void addBuddy(BuddyInfo newBuddy)
+	{
+		if(newBuddy != null)
+		{
+			this.abList.add(newBuddy);
+		}
+		
+	}
+	
+	public void removeBuddy(int index)
+	{
+		if(index >=0 && index < abList.size())
+		{
+			abList.remove(index);
+		}
+	}
+}
---
+++ src/BuddyInfo.java
@@ -0,0 +1,47 @@
+
+public class BuddyInfo extends AddressBook {
+	
+	private String bName;
+	private String bAddy;
+	private String bPhone;
+	
+	public BuddyInfo(String bName, String bAddy, String bPhone)
+	{
+		super();
+		this.bName = bName;
+		this.bAddy = bAddy;
+		this.bPhone = bPhone;
+	}
+	public static void main(String[] args) {
+
+		System.out.println("Hello!");
+		BuddyInfo bi = new BuddyInfo("Tommy","123 Address","867-5309");
+		System.out.println(bi.bName+" "+bi.bPhone);
+	}
+
+	public String getbName() {
+		return bName;
+	}
+
+	public void setbName(String bName) {
+		this.bName = bName;
+	}
+
+	public String getbAddy() {
+		return bAddy;
+	}
+
+	public void setbAddy(String bAddy) {
+		this.bAddy = bAddy;
+	}
+
+	public String getbPhone() {
+		return bPhone;
+	}
+
+	public void setbPhone(String bPhone) {
+		this.bPhone = bPhone;
+	}
+	
+
+}
